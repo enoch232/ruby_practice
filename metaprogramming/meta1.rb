b94a8f884613e91258619e749a10396e39323700
enoch232 <enoch232@gmail.com>
2016-07-18 22:58:48 -0700

    send

--- metaprogramming/meta1.rb
+++ metaprogramming/meta1.rb
@@ -6,11 +6,14 @@ class CoolStringClass
   def print_cool
   	puts "#{self.name} is cool."
   end
-  def self.print_cooler
-  	puts "Another cool"
+  def self.print_cooler(another)
+  	puts "Another cool #{another}"
   end
+  
 
 end
 newcool = CoolStringClass.new("hello")
 newcool.print_cool
-CoolStringClass.print_cooler
+CoolStringClass.print_cooler("net")
+CoolStringClass.send(:print_cooler, CoolStringClass.new("weird").name)
+
